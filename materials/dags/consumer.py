12b0317c89786a7153b7e3d2286bc92f513f3623
dhauss <davidkhauss@gmail.com>
2024-02-14 14:04:39 -0500

    second dataset added to consumer/producer pipeline

--- materials/dags/consumer.py
+++ materials/dags/consumer.py
@@ -4,11 +4,12 @@ from airflow.decorators import dag, task
 from datetime import datetime
 
 my_file = Dataset("/tmp/my_file.txt")
+my_file_2 = Dataset("/tmp/my_file_2.txt")
 
 
 @dag(
     dag_id="consumer",
-    schedule=[my_file],
+    schedule=[my_file, my_file_2],
     start_date=datetime(2024, 1, 1),
     catchup=False
 )
@@ -18,4 +19,11 @@ def read_dataset():
         print(f.read())
 
 
+@task
+def read_dataset_2():
+    with open(my_file_2.uri, "r") as f:
+        print(f.read())
+
+
 read_dataset()
+read_dataset_2()
--- materials/dags/producer.py
+++ materials/dags/producer.py
@@ -4,6 +4,7 @@ from airflow.decorators import dag, task
 from datetime import datetime
 
 my_file = Dataset("/tmp/my_file.txt")
+my_file_2 = Dataset("/tmp/my_file_2.txt")
 
 
 @dag(
@@ -12,10 +13,13 @@ my_file = Dataset("/tmp/my_file.txt")
     start_date=datetime(2024, 1, 1),
     catchup=False
 )
-@task(outlets=[my_file])
+@task(outlets=[my_file, my_file_2])
 def update_dataset():
     with open(my_file.uri, "a+") as f:
-        f.write("producer update")
+        f.write("producer update 1")
+
+    with open(my_file_2.uri, "a+") as f:
+        f.write("producer update 2")
 
 
 update_dataset()
